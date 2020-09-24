d14e6435972b07eb06803caa5fb5b61338e89249
Gaurav Dubey <gauravd638@gmail.com>
2020-09-24 14:34:00 +0530

    final  css commit

--- src/App.js
+++ src/App.js
@@ -20,11 +20,16 @@ function App() {
    
   }, []);
   return  ( 
-  <div>
+    <div>
+    <header>
+      <input className= "search" type="search" placeholder="search..."/>
+    </header>
+  <div className="movie-container">
     {movies.length > 0 &&
     movies.map((movie) => 
-      <Movie key={movie.id} {...Movie} />
+      <Movie key={movie.id} {...movie} />
     )}
+  </div>
   </div>
   );
     
